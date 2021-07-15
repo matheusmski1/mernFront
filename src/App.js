dbb5641b8abe5804aaead9dbad94c9be321abe7a
matheus dos santos <matheus.santos@franq.com.br>
2021-07-15 14:28:08 -0300

    just saving

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect } from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux'
+import { getPosts } from './actions/posts'
+import nicoRobin from './images/nicorobin.jpg'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import useStyles from './styles'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [currentId, setCurrentId] = useState(null);
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    
+
+    useEffect(()=> {
+        dispatch(getPosts())
+    }, [dispatch])
+
+    return (
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Nico Robin Page</Typography>
+                <img className={classes.image} src={nicoRobin} alt="nicoRobin" height="60"></img>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts setCurrentId={setCurrentId}/>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form currentId={currentId}/>
+                        </Grid>
+
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
+    );
 }
 
 export default App;
